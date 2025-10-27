9af2a1c5f57b37f414c01fcdb65961af8d156206
Paul Spooren <mail@aparcar.org>
2025-10-27 16:24:32 +0100

    tests: wait until br-lan is really ready
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- tests/test_lan.py
+++ tests/test_lan.py
@@ -1,6 +1,15 @@
 from ipaddress import IPv4Interface
+from time import sleep
 
 from conftest import ubus_call
+
+def test_lan_wait_for_link_ready(shell_command):
+    for _ in range(60):
+        if shell_command.run("dmesg | grep 'br-lan: link becomes ready'")[2] == 0:
+            return
+        sleep(1)
+
+    assert False, "LAN interface did not come up within 60 seconds"
 
 
 def test_lan_wait_for_network(shell_command):
@@ -17,3 +26,6 @@ def test_lan_interface_address(shell_command):
     assert shell_command.get_ip_addresses("br-lan")[0] == IPv4Interface(
         "192.168.1.1/24"
     )
+
+def test_lan_interface_has_neighbor(shell_command):
+    assert "DUP!" in "\n".join(shell_command.run("ping -c 3 ff02::1%br-lan")[0])
